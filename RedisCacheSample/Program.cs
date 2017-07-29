f77b8e061dbf7066bc5b00cb9593ced0e8478e13
manishchopra@myself.com <manishchopra@myself.com>
2017-07-29 08:30:32 +0100

    Add project files.

---
+++ RedisCacheSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCacheSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConnectionMultiplexer.Connect("YOUR CONNECTION STRING");
+            var db = connection.GetDatabase();
+
+            // Add something to the cache
+            db.StringSet("key", "value");
+
+            // Get value from cache
+            var value = db.StringGet("key");
+
+            Console.WriteLine("Value = " + value);
+
+            Console.ReadKey();
+
+        }
+    }
+}
